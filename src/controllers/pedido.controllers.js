c898ceae43bf80dd0e6b8f83a9d43d3a18259191
Diego <consultoresartificialbrain@gmail.com>
2021-05-03 11:52:51 -0500

    agregando toso

--- src/controllers/pedido.controllers.js
+++ src/controllers/pedido.controllers.js
@@ -159,15 +159,12 @@ pedidoCtrl.generatePedidoPagado = async (req,res) => {
                     ${pedidos}
                 </table>
                 <h3 style=" margin:auto; margin-left: 360px;"><strong>Sub total: </strong>$ ${subTotal}</h3>
-                ${politicas.length > 0 ? `
-                    <h3 style=" margin:auto; margin-left: 360px;"><strong>Costo de envio: </strong>$ ${politicas[0].costoEnvio}</h3>
-                    ${subTotal >= politicas[0].promocionEnvio ? 
-                    `<h3 style=" color: #CC2300; margin:auto; margin-left: 360px;"><strong>Descuento: </strong>- $${politicas[0].descuento}</h3>`    
-                    :"" }
-                    <h3 style=" color: #2DD703; margin:auto; margin-left: 360px;"><strong>Total: </strong>$ ${pedidoPopulate.total}</h3>`
-                :`
-                    <h3 style=" color: #2DD703; margin:auto; margin-left: 360px;"><strong>Total: </strong>$ ${pedidoPopulate.total}</h3>
-                `}
+                <h3 style=" margin:auto; margin-left: 360px;"><strong>Costo de envio: </strong>$ ${politicas[0].costoEnvio}</h3>
+                ${subTotal >= politicas[0].promocionEnvio ? 
+                `<h3 style=" color: #CC2300; margin:auto; margin-left: 360px;"><strong>Descuento: </strong>- $${politicas[0].descuento}</h3>`    
+                :"" }
+                <h3 style=" color: #2DD703; margin:auto; margin-left: 360px;"><strong>Total: </strong>$ ${pedidoPopulate.total}</h3>
+                
             </div>
             <div style="margin:auto; max-width: 550px; height: 100px;">
                 <p style="padding: 10px 0px;">Ya estamos trabajando para mandar tu pedido, si tienes alguna duda no dudes en contactarnos.</p>
